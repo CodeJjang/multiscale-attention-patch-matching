ec97fe158d7edcb1a87c8b68295c10c25af023c7
Aviad Moreshet <aviad10m@gmail.com>
2020-10-11 18:36:54 +0300

    Modified L2 norm to be applied to last dimension, dim=1 didnt work for multidim normalization

--- layers/L2Norm.py
+++ layers/L2Norm.py
@@ -8,6 +8,6 @@ class L2Norm(nn.Module):
         self.eps = 1e-10
 
     def forward(self, x):
-        norm = torch.sqrt(torch.sum(x * x, dim=1) + self.eps)
+        norm = torch.sqrt(torch.sum(x * x, dim=-1) + self.eps)
         x = x / norm.unsqueeze(-1).expand_as(x)
         return x
