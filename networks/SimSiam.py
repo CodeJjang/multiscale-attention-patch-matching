6a194ccb5518a9996b9abc14f9f35ee8894f880e
Aviad Moreshet <aviad10m@gmail.com>
2021-07-19 21:56:01 +0300

    Error 17.12

--- networks/SimSiam.py
+++ networks/SimSiam.py
@@ -20,12 +20,12 @@ class SimSiam(nn.Module):
     def forward(self, x1, x2):
         enc_output = self.encoder(x1, x2)
         z1, z2 = enc_output['Emb1'], enc_output['Emb2']
-        # z1, z2 = F.normalize(z1, dim=1, p=2), F.normalize(z2, dim=1, p=2)
         if self.training:
             z1, z2 = self.projector(z1), self.projector(z2)
             p1, p2 = self.predictor(z1), self.predictor(z2)
             loss = self.loss(p1, z2) / 2 + self.loss(p2, z1) / 2
             return loss
+        z1, z2 = F.normalize(z1, dim=1, p=2), F.normalize(z2, dim=1, p=2)
         res = {
             'Emb1': z1,
             'Emb2': z2
